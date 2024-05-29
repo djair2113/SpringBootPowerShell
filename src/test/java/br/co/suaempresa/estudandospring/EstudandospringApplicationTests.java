bebd2a21cf1755d9408b400ea952a74ff297445a
Aluno <https://github.com/djair2113/SpringBootPowerShell.git>
2024-05-28 22:28:18 -0300

    Power shell e Spring boot

---
+++ src/test/java/br/co/suaempresa/estudandospring/EstudandospringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.co.suaempresa.estudandospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
